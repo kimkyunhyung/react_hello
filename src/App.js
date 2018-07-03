e989b0af4aec4054756dca8e39a0198413bbb1de
김균형 <kimbalce@gmail.com>
2018-07-03 15:56:38 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const goals = [
+  "1. react 개발에 필요한 환경을 구축한다.",
+  "2. react 개발에 필요한 환경을 구축한다.",
+  "3. react 개발에 필요한 IDE를 읽힌다."
+];
+
+const LectureGoalList = props => {
+  return (
+    <div>
+      <div> {props.title} </div>
+      <ul>{props.items.map((item, idx) => <li key={idx}>{item}</li>)}</ul>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
@@ -13,6 +28,8 @@ class App extends Component {
         <p className="App-intro">
           To , edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <LectureGoalList items={goals} title={"강의목표!!!"} />
       </div>
     );
   }
---
+++ src/es5.js
@@ -0,0 +1,4 @@
+const f = ()=>{
+  return 1;
+}
+
