5eff6f7ed3d162444aa5663c12cce62094b96400
김균형 <kimbalce@gmail.com>
2018-07-04 15:54:49 +0900

    day2 15:00...  데이타 변경시 랜더링 하기... shouldComponentUpdate

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,28 +1,43 @@
 import React, { Component } from "react";
 import moment from "moment";
 import "moment/locale/ko";
+const TIME_FORMAT = "A h:mm";
 
 class Timer extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    this.nTimer = setInterval(this.handleTick, 1000);
+    this.state = {
+      date: moment()
+    };
+    this.nTimer = setInterval(() => {
+      this.setState({
+        date: moment()
+      });
+    }, 1000);
   }
 
-  handleTick = () => {
-    console.log(this.nTime);
-    this.forceUpdate();
-  };
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      this.state.date.format(TIME_FORMAT) === nextState.date.format(TIME_FORMAT)
+    )
+      return false;
+    else return true;
+  }
 
   render() {
     console.log("rener...");
+
+    const { expireDate } = this.props;
+    const { date } = this.state;
+
+    if (moment(expireDate) < date) {
+      return <div> 종료되었습니다 </div>;
+    }
     return (
       <div>
-        <div>현재시간: {moment().format("YYYY-MM-DD A hh:mm")}</div>
-        <div>
-          강의종료시간 {moment(this.props.expireDate).fromNow()}에 강의
-          종료합니다.
-        </div>
+        <div>현재시간: {date.format(TIME_FORMAT)}</div>
+        <br />
+        <div>오늘은 {moment(expireDate).fromNow()}에 강의 종료합니다.</div>
       </div>
     );
   }
