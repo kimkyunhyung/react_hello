8eb099914747ee8a5642a37cd2bafff467450027
김균형 <kimbalce@gmail.com>
2018-07-05 16:46:27 +0900

    Not Finish .. but...

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import Todos from "./components/Todos";
 import Timer from "./components/Timer";
 import Header from "./components/Header";
 
+import { Input } from "antd";
+
 import logo from "./logo.svg";
 import moment from "moment";
 
+import axios from "axios";
 import "./App.css";
-//import "antd/dist/antd.css";
+import "antd/dist/antd.css";
 
 const goals = [
   { title: "1. react 개발에 필요한 환경을 구축한다.", completed: true },
   { title: "2. react 개발에 필요한 Javascript를 구축한다.", completed: true },
   { title: "3. react 개발에 필요한 IDE를 읽힌다.", completed: true },
-  { title: "4. react 개발에 필요한 IDE를 읽힌다.", completed: false }
+  { title: "4. react 개발에 필요한 IDE를 읽힌다.", completed: true }
 ];
 
 class App extends Component {
   state = {
+    data: [],
     isExpired: false,
     dateStr: "2018-07-05T17:00:00+09:00"
   };
@@ -37,6 +41,10 @@ class App extends Component {
   };
 
   componentDidMount() {
+    axios.get("http://api.tvmaze.com/search/shows?q=girls").then(({ data }) => {
+      this.setState({ data });
+    });
+
     setInterval(() => {
       this.setState({
         dateStr: "2018-07-05T16:00:00+09:00"
@@ -61,11 +69,7 @@ class App extends Component {
           <h1 className="App-title">hello to React</h1>
         </header>
     */}
-
-        <p className="App-intro">
-          To , edit <code>src/App.js</code> and save to reload.
-        </p>
-
+        <br />
         <Todos items={goals} title={"강의목표!!!"} />
         {!isExpired && (
           <Timer
@@ -75,6 +79,8 @@ class App extends Component {
             onComplete={this.handleClick}
           />
         )}
+
+        <img src={this.state.data} alt="TEST" />
       </div>
     );
   }
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,17 +1,51 @@
 import React, { Component } from "react";
-import { Checkbox } from "antd";
+import { Checkbox, Input, Icon } from "antd";
+import "./Todos.less";
 
-const ListItem = ({ title, completed }) => (
-  <li>
-    <Checkbox defaultChecked={completed}>{title}</Checkbox>
-  </li>
-);
+class Todos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [...props.items]
+    };
+  }
 
-const Todos = props => (
-  <div className="Todos">
-    <div> {props.title} </div>
-    <ul>{props.items.map((item, idx) => <ListItem key={idx} {...item} />)}</ul>
-  </div>
-);
+  handlePress = e => {
+    const { items } = this.state;
+    items.push({
+      title: e.target.value,
+      completed: false
+    });
+  };
+
+  handleDelete(e, item, idx) {
+    let { items } = this.state;
+
+    items = items.splice(idx, 1);
+
+    this.setState({
+      items: [...items]
+    });
+  }
+
+  render() {
+    const { title, items } = this.props;
+    return (
+      <div className="Todos">
+        <div> {title} </div>
+        <Input OnPressEnter={this.handlePress} />
+
+        <ul>
+          {items.map((item, idx) => (
+            <li key={idx}>
+              <Icon className="icon" type="delete" />
+              <Checkbox defaultChecked={item.completed}>{item.title}</Checkbox>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Todos;
